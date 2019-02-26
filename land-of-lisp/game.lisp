c516c9ef7261d7fe8d6d9bfbfe32b5600067740b
hiramekun <thescript1210@gmail.com>
2019-02-26 10:08:41 +0900

    Add describe-paths

--- land-of-lisp/game.lisp
+++ land-of-lisp/game.lisp
@@ -12,9 +12,12 @@
 (defparameter *edges* '((living-room (garden west door)
                                      (attic upstairs ladder))
                         (garden (living-room east door))
-                        (attic (living-room downstairs ladder))
-                        ))
+                        (attic (living-room downstairs ladder))))
 
 (defun describe-path (edge)
   `(there is a ,(caddr edge) going,(cadr edge) from here.)
+  )
+
+(defun describe-paths (location edges)
+  (apply #'append (mapcar #'describe-path (cdr (assoc location edges))))
   )
